d7ca292da571ff1528fec91ad4dbf019c8db1b6f
MichaelOlanoseh <mike232lkw@gmail.com>
2026-06-15 06:50:24 +0100

    Group4 project files add

--- test_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'monime_config.php';
-if (defined('MONIME_TOKEN')) {
-    echo "✅ Config is working! Token is set.";
-} else {
-    echo "❌ Config is NOT working. PHP cannot see the constants.";
-}
-?>
